0762a4a154e1436f10a5fe1fb62142f00d742b90
Guillermo Olvera López <73192041+GuillermoOlvera@users.noreply.github.com>
2023-11-09 00:17:44 -0600

    comentarios agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Importa los componentes necesarios de React y Material-UI, así como los estilos de makeStyles
 import { BeneficiariesForm } from "./components/Beneficiary";
 import { Login } from "./components/Login";
 import { Patient } from "./components/Patient";
@@ -5,6 +6,7 @@ import { PatientData } from "./components/PatientData";
 import { Container, CssBaseline, Paper, Typography } from "@mui/material";
 import { makeStyles } from "@material-ui/core/styles";
 
+// Define estilos personalizados con makeStyles
 const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
@@ -21,9 +23,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// Componente principal de la aplicación
 const App = () => {
+  // Utiliza los estilos definidos con makeStyles
   const classes = useStyles();
 
+  // Renderiza la interfaz de usuario con varios componentes, como Login, Patient, PatientData y BeneficiariesForm
   return (
     <Container component="main" maxWidth="xs" className={classes.container}>
       <CssBaseline />
